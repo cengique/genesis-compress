e4c03fad89ce6501338f73933c9a227881bd7aa3
Cengiz Gunay <cengique@users.sf.net>
2009-11-06 22:50:12 +0000

    Fixed test program and updated doc for version test.

--- test/genflactest.m
+++ test/genflactest.m
@@ -1,9 +1,11 @@
 
+addpath('../src/');
+
 % load and plot files
 
 data1 = readgenesis('genesis-single-chan.bin', 1);
 data2 = readgenesis('genesis-single-chan.test.bin', 1);
-data3 = readgenesis16bit('genesis-single-chan.test.genflac');
+data3 = readgenflac('genesis-single-chan.test.genflac');
 t=1:20000;
 figure; plot(t, data1, t, data2, t, data3); legend('orig', 'uncomp', 'comp');
 
